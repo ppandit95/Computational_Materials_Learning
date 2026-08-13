cefe5e70ca547eaad6b202660841f30bc9c2f799
ppandit95 <pushkarpandit95@gmail.com>
2026-08-13 09:42:22 +0530

    Day 7:Consolidate and verify week 1 tooolchain

--- cpp_foundations/diagnostic_exercise.cpp
+++ cpp_foundations/diagnostic_exercise.cpp
@@ -7,6 +7,7 @@
 		double R = 8.314;
 		int T[4] = {1000,1100,1200,1300};
 		double D[4];
+		double scientific_check_failed = 0.0;
 		for(int i=0;i<4;i++)
 		{
 			D[i] = D0*std::exp(-Q/(R*T[i]));
@@ -15,11 +16,18 @@
 			if(i>0)
 			{
 				if((D[i]-D[i-1])>0)
-				std::cout<<"The Diffusivity seems to follow the trend at position "<< i <<std::endl;
-				else
-				std::cout<<"There seems to be issues physics that needs to be looked into."<<std::endl;
+					std::cout<<"The Diffusivity seems to follow the trend at position "<< i <<std::endl;
+				else{
+					std::cout<<"There seems to be issues physics that needs to be looked into."<<std::endl;
+					scientific_check_failed = 1.0;
+				}
 				std::cout<<"--------------------------------------------------------------"<<std::endl;
 			}
+		}
+			if (scientific_check_failed)
+		{
+    			std::cout<<"There is soomething wrong with the formula of arrhenius equation please recheck it!!!"<<std::endl;
+    			return scientific_check_failed;
 		}
 		return 0;
 	}

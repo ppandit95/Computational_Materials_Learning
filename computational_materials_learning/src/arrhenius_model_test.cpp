e5fc7bd6c32437bb63377b55df23bc2a199f0835
ppandit95 <pushkarpandit95@gmail.com>
2026-08-21 10:09:03 +0530

    day 15: encapsulate Arrhenius parameters in a class

---
+++ computational_materials_learning/src/arrhenius_model_test.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<stdexcept>
+#include"arrhenius_model.hpp"
+int main(){
+	ArrheniusModel AModel(1.0e-4,250000.0,8.314);
+	std::cout<<"---------------------------Case 1: Valid Model+Valid T----------------------------------------------"<<std::endl;
+	std::cout<<"Corresponding Diffusivity at 1000K is : " <<AModel.diffusivity(1000.0)<<std::endl;
+	std::cout<<"---------------------------Case 2: T<0 ----------------------------------------------"<<std::endl;
+	try{
+		AModel.diffusivity(-1000.0);
+	}
+	catch(const std::invalid_argument& e)
+	{
+    		std::cerr << "Input error: " << e.what() << '\n';
+	}
+	std::cout<<"---------------------------Case 3: Invalid Model Parameter ----------------------------------------------"<<std::endl;
+	try{
+		ArrheniusModel bad_Model(1.0e-4,250000.0,-8.314);
+	}
+	catch(const std::invalid_argument& e)
+	{
+    		std::cerr << "Object Parameter error: " << e.what() << '\n';
+	}
+	return 0;
+}
+
+	
